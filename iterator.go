611df19131bc906f02cc45512e4ae99344031e77
Sarthak Shastri <55555570+sarthak815@users.noreply.github.com>
2022-10-17 00:37:31 +0530

    Revert "Partial nodeIterator interface Implemented"
    This reverts commit 174a74cd0788aa3e3d38001237db37c8e5426105.

--- iterator.go
+++ iterator.go
@@ -1,8 +1,6 @@
 package smt
 
 import (
-	"bytes"
-	"errors"
 	"hash"
 )
 
@@ -33,22 +31,17 @@ func (it *Iterator) Next() bool {
 	}
 }
 
-//Prove generates Merkle proof of the leaf positioned on
-func (it *Iterator) Prove() [][]byte {
-	return it.nodeIt.LeafProof()
-}
-
 //NodeIterator is an iterator that traverses the trie pre-order
 type NodeIterator interface {
 	//Next moves the iterator to the next node. The boolean value is false
 	//any other child nodes present will be skipped
-	Next(bool) bool
+	Next(bool)
 
 	//Error returns the error status of iterator
 	Error() error
 
 	//Hash returns the hash of the current node
-	Hash() []byte
+	Hash() hash.Hash
 
 	//Parent returns the hash of the next available parent node
 	Parent() hash.Hash
@@ -89,111 +82,4 @@ type nodeIterator struct {
 	stack []*nodeIteratorState
 	path  []byte
 	err   error
-}
-
-func (n nodeIterator) Error() error {
-	//TODO implement me
-	panic("implement me")
-}
-
-//Hash returns the hash of nodes iterated up until method is called
-func (n nodeIterator) Hash() []byte {
-	if len(n.stack) == 0 {
-		return treeHasher.placeholder()
-	}
-
-	return n.trie.th.hasher.Sum(n.stack[len(n.stack)-1].node)
-}
-
-func (n nodeIterator) Parent() hash.Hash {
-	//TODO implement me
-	panic("implement me")
-}
-
-func (n nodeIterator) Path() []byte {
-	//TODO implement me
-	panic("implement me")
-}
-
-func (n nodeIterator) NodeBlob() MapStore {
-	//TODO implement me
-	panic("implement me")
-}
-
-func (n nodeIterator) Leaf() bool {
-	//TODO implement me
-	panic("implement me")
-}
-
-func (n nodeIterator) LeafKey() []byte {
-	//TODO implement me
-	panic("implement me")
-}
-
-func (n nodeIterator) LeafBlob() []byte {
-	//TODO implement me
-	panic("implement me")
-}
-
-func (n nodeIterator) LeafProof() [][]byte {
-	//TODO implement me
-	panic("implement me")
-}
-
-func (n nodeIterator) seek(start []byte) error {
-
-}
-
-// errIteratorEnd is stored in nodeIterator.err when iteration is done.
-var errIteratorEnd = errors.New("end of iteration")
-
-// seekError is stored in nodeIterator.err if the initial seek has failed.
-type seekError struct {
-	key []byte
-	err error
-}
-
-//newNodeIterator creates a new iterator for the sparse merkle tree provided
-func newNodeIterator(smt *SparseMerkleTree, start []byte) NodeIterator {
-	if bytes.Compare(smt.root, smt.th.placeholder()) == 0 {
-		return &nodeIterator{
-			trie: smt,
-			err:  errIteratorEnd,
-		}
-	}
-	n := &nodeIterator{trie: smt}
-	n.err = n.seek(start)
-	return n
-}
-
-// Next moves the iterator to the next node, returning whether there are any
-// further nodes. In case of an internal error this method returns false and
-// sets the Error field to the encountered failure. If `descend` is false,
-// skips iterating over any subnodes of the current node.
-//func (n *nodeIterator) Next(descend bool) bool {
-//	if n.err == errIteratorEnd {
-//		return false
-//	}
-//	if seek, ok := n.err.(seekError); ok {
-//		if n.err = n.seek(seek.key); n.err != nil {
-//			return false
-//		}
-//	}
-//	// Otherwise step forward with the iterator and report any errors.
-//	state, parentIndex, path, err := it.peek(descend)
-//	n.err = err
-//	if n.err != nil {
-//		return false
-//	}
-//	n.push(state, parentIndex, path)
-//	return true
-//}
-//push function pushes the state nodeIteratorState into the stack that is used
-//to maintain state data along with nodeIterator
-func (n nodeIterator) push(state *nodeIteratorState, parentIndex *int, path []byte) {
-	n.path = path
-	n.stack = append(n.stack, state)
-	if parentIndex != nil {
-		*parentIndex++
-	}
 }
